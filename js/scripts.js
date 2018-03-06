5ec9420a41d22ec5c04e7c6f232f805d75a44b08
Nyabuto Vice <nyabutovice@gmail.com>
2018-03-06 15:15:39 +0300

    add div classes and modify the html and scripts file

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function rollDice(){
+  var die1=document.getElementById("die1");
+  var die2=document.getElementById("die2");
+  var status=document.getElementById("status");
+  var d1=Math.floor(Math.random()*6)+1;
+  var d2=Math.floor(Math.random)*6)+1;
+  var diceTotal=d1+d2;
+  die1.innerHTML=d2;
+  die2.innerHTML=d1;
+  status.innerHTML="You Rolled" + diceTotal ".";
+  if(d1==d2){
+    status.innerHTML += "Doubles! You get free turn!";
+  }
+}
